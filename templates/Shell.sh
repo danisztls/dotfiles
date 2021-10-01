d565651d1cb5c62178c6c6b93b41be33f9b5d8d5
Daniel Souza <me@posix.dev.br>
2021-10-01 02:36:06 -0300

    Bash: write text decorators boilerplate

--- templates/Shell.sh
+++ templates/Shell.sh
@@ -4,3 +4,18 @@
 
 # @author: Daniel Souza <me@posix.dev.br>
 # @license: MIT
+
+# Text Decorators
+reset="\e[0m"
+strong="\e[1;39m"
+red="\e[1;31m"
+yellow="\e[1;33m"
+green="\e[1;32m"
+blue="\e[1;34m"
+
+printf "${strong}Colors:${reset} \
+${red}%s${reset}, \
+${yellow}%s${reset}, \
+${green}%s${reset}, \
+${blue}%s${reset}.\n" \
+"Red" "Yellow" "Green" "Blue"
