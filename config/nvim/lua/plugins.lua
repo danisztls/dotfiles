863b343fdf681b232ab052b55ab1cb46fa79aa94
Daniel Souza <me@posix.dev.br>
2023-05-04 11:09:02 -0300

    nvim: set up ai plugins

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,53 +1,77 @@
--- returns the require for use in `config` parameter of lazy's use
--- expects the name of the config file
 function get_setup(name)
   return string.format('require("setup/%s")', name)
 end
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Manage itself 
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
 
   -- Browsing
   use {
-    'nvim-telescope/telescope.nvim', 
-    requires = {'nvim-lua/plenary.nvim'},
+    "nvim-telescope/telescope.nvim", 
+    requires = {"nvim-lua/plenary.nvim"},
     config = get_setup("telescope")
   }
-  use 'tpope/vim-vinegar'
+  use "tpope/vim-vinegar"
 
   -- Visual
-  use 'nvim-tree/nvim-web-devicons'
-  use {'dracula/vim', as = 'dracula'}
+  use "nvim-tree/nvim-web-devicons"
+  use {"dracula/vim", as = "dracula"}
   use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'nvim-tree/nvim-web-devicons', opt = true},
+    "nvim-lualine/lualine.nvim",
+    requires = {"nvim-tree/nvim-web-devicons", opt = true},
     config = get_setup("lualine")
   }
   use {"lukas-reineke/indent-blankline.nvim", config = get_setup("indent")}
-  use {'edluffy/hologram.nvim', config = get_setup("hologram")}
-  use {'rrethy/vim-hexokinase', run = 'make hexokinase', config = get_setup("hexokinase")}
+  use "dstein64/nvim-scrollview"
+  use {"rrethy/vim-hexokinase", run = "make hexokinase", config = get_setup("hexokinase")}
+  use {"edluffy/hologram.nvim", config = get_setup("hologram")}
 
   -- Code
-  use {'dense-analysis/ale', config = get_setup("ale")}
-  use {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', config = get_setup("treesitter")}
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
+  use {"dense-analysis/ale", config = get_setup("ale")}
+  use {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = get_setup("treesitter")}
+  use "tpope/vim-surround"
+  use "tpope/vim-commentary"
 
   -- Git
-  use {'tpope/vim-fugitive', config = get_setup("git-fugitive")}
-  use 'tpope/vim-rhubarb'
-  use 'junegunn/gv.vim'
-  use {'rhysd/git-messenger.vim', config = get_setup("git-messenger")}
-  use {'lewis6991/gitsigns.nvim', config = get_setup("git-signs")}
+  use {"tpope/vim-fugitive", config = get_setup("git-fugitive")}
+  use "tpope/vim-rhubarb"
+  use "junegunn/gv.vim"
+  use {"rhysd/git-messenger.vim", config = get_setup("git-messenger")}
+  use {"lewis6991/gitsigns.nvim", config = get_setup("git-signs")}
 
   -- Markdown
-  use {'plasticboy/vim-markdown', ft = 'markdown'}
-  -- use {'mzlogin/vim-markdown-toc', ft = 'markdown'}
+  use {"plasticboy/vim-markdown", ft = "markdown"}
+  -- use {"mzlogin/vim-markdown-toc", ft = "markdown"}
 
   -- Misc
-  -- use 'tpope/vim-abolish'
-  use {'godlygeek/tabular', config = get_setup("tabular")}
-  use 'cespare/vim-toml'
-  use 'dstein64/nvim-scrollview'
+  -- use "tpope/vim-abolish"
+  use {"godlygeek/tabular", config = get_setup("tabular")}
+  use "cespare/vim-toml"
+
+  -- AI
+  use {"github/copilot.vim", config = get_setup("copilot")}
+
+  -- use({
+  -- "jackMort/ChatGPT.nvim",
+  --   config = function()
+  --     require("chatgpt").setup()
+  --   end,
+  --   requires = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim"
+  --   }
+  -- })
+  
+  use({
+    "dpayne/CodeGPT.nvim",
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("codegpt.config")
+    end
+  })
 end)
---
+++ config/nvim/lua/setup/copilot.lua
@@ -0,0 +1,8 @@
+vim.g.copilot_filetypes = {
+  ["*"] = false,
+  ["javascript"] = true,
+  ["typescript"] = true,
+  ["rust"] = true,
+  ["go"] = true,
+  ["python"] = true,
+}
