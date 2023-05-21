3f905ab4f639c1e83e54c2f7f3ec7e92337bc843
Daniel Souza <me@posix.dev.br>
2023-05-21 15:41:53 -0300

    nvim(lsp): replace pylyzer with pylsp server

--- config/nvim/lua/setup/lsp.lua
+++ config/nvim/lua/setup/lsp.lua
@@ -5,8 +5,19 @@ local lsp_config = {
   capabilities = require('cmp_nvim_lsp').default_capabilities()
 }
 
+lsp.lua_ls.setup(lsp_config)
 lsp.eslint.setup(lsp_config)
 lsp.cssls.setup(lsp_config)
 lsp.html.setup(lsp_config)
-lsp.pylyzer.setup(lsp_config)
-lsp.lua_ls.setup(lsp_config)
+lsp.pylsp.setup(lsp_config)
+
+-- pylyzer sucks (may-23)
+-- lsp.pylyzer.setup({
+--   capabilities = require('cmp_nvim_lsp').default_capabilities(),
+--   python = {
+--     checkOnType = false,
+--     diagnostics = false,
+--     inlayHints = false,
+--     smartCompletion = true
+--   }
+-- })
