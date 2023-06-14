bd2473ab5136c69f542bd19c8e6068f5e4ae8399
Daniel Souza <me@posix.dev.br>
2023-06-14 01:24:49 -0300

    nvim: ensure Packer is installed (bootstrap)

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+-- Bootstrap Packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -168,4 +182,8 @@ return require("packer").startup(function(use)
     "folke/which-key.nvim",
     config = get_setup("which-key")
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
