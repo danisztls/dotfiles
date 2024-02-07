38734099094abacec1c818c3bb05a4e6fbcdae5d
Daniel Souza <me@posix.dev.br>
2024-02-07 14:23:30 -0300

    nvim: rewrite functions & format

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,58 +1,51 @@
-require("options")
-require("keymaps")
-require("plugins")
-
--- vim.cmd('source ~/.config/nvim/legacy.vim')
-
--- WEB
--- autocmd FileType html css
+require "options"
+require "keymaps"
+require "plugins"
 
 -- FUNCTIONS
--- convert rows of numbers or text (as if pasted from excel column) to a tuple
-function _G.ToTuple()
-   vim.cmd('silent execute "'..vim.fn.line("'<")..','..vim.fn.line("'>")..'s/^/\'/"')
-   vim.cmd('silent execute "'..vim.fn.line("'<")..','..vim.fn.line("'>")..'s/$/\',/"')
-   vim.cmd('silent execute "'..vim.fn.line("'<")..','..vim.fn.line("'>")..'join"')
-   vim.cmd('normal I(')
-   vim.cmd('normal $xa)')
+-- Convert rows of space separated values to a tuple
+function ToTuple()
+  local start_line = vim.fn.line "'<"
+  local end_line = vim.fn.line "'>"
+
+  -- add single quotes at the start and end of each line
+  vim.cmd('silent execute "' .. start_line .. "," .. end_line .. "s/^/'/\"")
+  vim.cmd('silent execute "' .. start_line .. "," .. end_line .. "s/$/',/\"")
+
+  -- join all lines into a single line
+  vim.cmd('silent execute "' .. start_line .. "," .. end_line .. 'join"')
+
+  -- add parentheses at the start and end of the line
+  vim.cmd "normal I("
+  vim.cmd "normal $xa)"
 end
 
--- convert a simple code span to a nice shell code fence
-function _G.ToShell()
-   vim.cmd('silent execute "s/^`/```sh\\r/"')
-   vim.cmd('silent execute "s/`$/\\r```/"')
+-- Wrap selection in a code fence
+function ToCodeFence()
+  vim.cmd "normal x^I```"
+  vim.cmd "normal $a```"
+  vim.cmd "normal jp"
 end
+vim.api.nvim_set_keymap("n", "<leader>s", ":lua ToCodeFence()<CR>", { noremap = true, silent = true })
 
--- remove trailing whitespaces
-function _G.CleanUp(...)
-   local first, last
-   if arg.n == 0 then
-     first, last = vim.fn.line("'<"), vim.fn.line("'>")
-   else
-     first, last = arg[1], arg[2]
-   end
-   for lnum=tonumber(first),tonumber(last) do
-       local line = vim.fn.getline(lnum)
-       line = line:gsub('%s+$', '')
-       vim.fn.setline(lnum, line)
-   end
+function CleanTrailingWhitespaces()
+  vim.cmd "%s/\\s\\+$//e"
 end
-vim.api.nvim_set_keymap('n', '<leader>x', ':set opfunc=CleanUp<CR>g@', {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>x", ":lua CleanTrailingWhitespaces()<CR>", { noremap = true, silent = true })
 
--- Autocommand
+-- AUTOCOMMANDS
 -- prevent including autocommands more than once
 local autocommands_loaded = false
 if not autocommands_loaded then
-   autocommands_loaded = true
-   vim.cmd('au BufNewFile,BufRead *.tsv set filetype=tsv') -- tab-separated values
-   vim.cmd('au BufNewFile,BufRead *.json set conceallevel=0')
+  autocommands_loaded = true
+  vim.cmd "au BufNewFile,BufRead *.tsv set filetype=tsv" -- tab-separated values
+  vim.cmd "au BufNewFile,BufRead *.json set conceallevel=0"
 
-   vim.cmd([[
-       augroup wikimedia
-       au!
-       au BufNewFile,BufRead *work/wmf/* set noexpandtab tabstop=4 shiftwidth=4
-       " au BufNewFile,BufRead *work/wmf/**/*.{html,js,css,scss,php} set noet ts=4 sw=4
-       augroup END
-   ]])
+  vim.cmd [[
+    augroup Wikimedia
+      au!
+      au BufNewFile,BufRead *work/wmf/* set noexpandtab tabstop=4 shiftwidth=4
+      " au BufNewFile,BufRead *work/wmf/**/*.{html,js,css,scss,php} set noet ts=4 sw=4
+    augroup END
+  ]]
 end
-
